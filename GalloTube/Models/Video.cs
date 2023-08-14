a01cf6310b4b3216f9af3a98ac28914996581f99
Guilherme Bispo e Gabriel <guilherme19.b07@gmail.com>
2023-08-14 09:36:45 -0300

    iniciando o projeto GalloTube

---
+++ GalloTube/Models/Video.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace GalloTube.Models;
+
+[Table("Video")]
+public class Video
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Display(Name = "Nome")]
+    [Required(ErrorMessage = "O Nome é obrigatório")]
+    [StringLength(100, ErrorMessage = "O nome deve possuir no máximo 100 caracteres")]
+    public string Name { get; set; }
+
+    [Display(Name = "Sinopse")]
+    [Required(ErrorMessage = "A Sinopse é obrigatória")]
+    [StringLength(8000, ErrorMessage = "A Sinopse deve possuir no máximo 5000 caracteres")]
+    public string Synopsis { get; set; }
+
+    [Column(TypeName = "Year")]
+    [Display(Name = "Ano de Estreia")]
+    [Required(ErrorMessage = "O Ano de Estreia é obrigatório")]
+    public Int16 MovieYear { get; set; }
+
+    [Display(Name = "Duração (em minutos)")]
+    [Required(ErrorMessage = "A Duração é obrigatória")]
+    public Int16 Duration { get; set; }
+
+    [Display(Name = "Classificação Etária")]
+    [Required(ErrorMessage = "A Classificação Etária é obrigatória")]
+    public byte AgeRating { get; set; }
+
+    [StringLength(200)]
+    [Display(Name = "Foto")]
+    public string Image { get; set; }
+
+    [NotMapped]
+    [Display(Name = "Duração")]
+    public string HourDuration { get {
+        return TimeSpan.FromMinutes(Duration) .ToString(@"%h'h 'mm'min'");
+    }}
+
+    public ICollection<VideoTag> Tag { get; set; }
+}
